38a3ee3c67b991c84efe4c0358f777fa419be456
Matt Hand <mmhand123@gmail.com>
2026-01-10 10:14:31 -0500

    add version

--- src/main.zig
+++ src/main.zig
@@ -1,12 +1,15 @@
 const std = @import("std");
 const clap = @import("clap");
 
+const VERSION = "0.0.1";
+
 pub fn main() !void {
     var gpa = std.heap.DebugAllocator(.{}){};
     defer _ = gpa.deinit();
 
     const params = comptime clap.parseParamsComptime(
         \\-h, --help      Display this help and exit.
+        \\-v, --version   Print version information and exit.
     );
 
     var diag: clap.Diagnostic = .{};
@@ -22,5 +25,7 @@ pub fn main() !void {
 
     if (res.args.help != 0) {
         return clap.helpToFile(.stderr(), clap.Help, &params, .{});
+    } else if (res.args.version != 0) {
+        std.debug.print("fzm v{s}\n", .{VERSION});
     }
 }
