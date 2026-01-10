b8b89bcddcbc8be1a48e87bf9663dc583a10adfc
Matt Hand <mmhand123@gmail.com>
2026-01-10 10:49:15 -0500

    switch to arena allocator

--- src/install.zig
+++ src/install.zig
@@ -3,7 +3,6 @@ const dirs = @import("dirs.zig");
 
 pub fn install(allocator: std.mem.Allocator, version: []const u8) !void {
     const data_dir = try dirs.getDataDir(allocator);
-    defer allocator.free(data_dir);
 
     std.debug.print("installing {s}\n", .{version});
     std.debug.print("exe_dir: {s}\n", .{data_dir});
--- src/main.zig
+++ src/main.zig
@@ -5,8 +5,10 @@ const installation = @import("install.zig");
 const VERSION = "0.0.1";
 
 pub fn main() !void {
-    var gpa = std.heap.DebugAllocator(.{}){};
-    defer _ = gpa.deinit();
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+
+    const allocator = arena.allocator();
 
     const params = comptime clap.parseParamsComptime(
         \\-h, --help      Display this help and exit.
@@ -15,21 +17,19 @@ pub fn main() !void {
     );
 
     var diag: clap.Diagnostic = .{};
-    var res = clap.parse(clap.Help, &params, clap.parsers.default, .{
+    const res = clap.parse(clap.Help, &params, clap.parsers.default, .{
         .diagnostic = &diag,
-        .allocator = gpa.allocator(),
+        .allocator = allocator,
     }) catch |err| {
         try diag.reportToFile(.stderr(), err);
         return;
     };
-
-    defer res.deinit();
 
     if (res.args.help != 0) {
         return clap.helpToFile(.stderr(), clap.Help, &params, .{});
     } else if (res.args.version != 0) {
         std.debug.print("fzm v{s}\n", .{VERSION});
     } else if (res.args.install) |version| {
-        try installation.install(gpa.allocator(), version);
+        try installation.install(allocator, version);
     }
 }
